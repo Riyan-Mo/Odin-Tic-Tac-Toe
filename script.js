3a2d6a7b5e36f9d6c5b5bc2df8581a64a916669c
Riyan911 <riyan8308151899@gmail.com>
2023-03-22 12:03:46 +0530

    Changed to factory functions

--- script.js
+++ script.js
@@ -2,20 +2,93 @@ const Player = function(symbol, isTurn){
   return { symbol, isTurn };
 }
 
-function setId(){
-  let index = 0;
-  const gameBoard = document.querySelector("#gameBoard");
-  Array.from(gameBoard.children).forEach(element => {
-    element.setAttribute('id', index);
-    index++;
-  });
-}  
 
-let Game = (function(){  
+const Game = () => {  
   let player1 = Player("X", true);
   let player2 = Player("O", false);
   let initialBoard = ["", "", "", "", "", "", "", "", ""];
   const gameBoard = document.querySelector("#gameBoard");
+
+  const setId = () => {
+    let index = 0;
+    const gameBoard = document.querySelector("#gameBoard");
+    Array.from(gameBoard.children).forEach(element => {
+      element.setAttribute('id', index);
+      index++;
+    });
+  };
+
+  const toDisplay = (p1, p2, piece, board) => {
+    pyTurn = playerTurn(p1, p2);
+    piece.textContent = pyTurn;
+    board[piece.getAttribute('id')] = pyTurn;
+    piece.disabled = true;
+    checkDraw();
+    checkWin(p1, p2, board);
+  };
+  
+  const playerTurn = (one, two) => {
+    return one.isTurn?changeTurn(one, two): changeTurn(two, one);
+  }
+
+  const changeTurn = (player1, player2) => {
+    const turnPara = document.querySelector("#turnPara");
+    player1.isTurn = false;
+    player2.isTurn = true;
+    turnPara.textContent = player2.symbol+"'s turn!";
+    return player1.symbol;
+  }
+
+  const checkWin = (one, two, board) => {
+    const win = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    for(let i = 0; i<win.length; i++){
+      let a = win[i][0];
+      let b = win[i][1];
+      let c = win[i][2];
+      symbolCheck(a, b, c, one.symbol, board);
+      symbolCheck(a, b, c, two.symbol, board);
+    }
+  }
+
+  const symbolCheck = (one, two , three, symbol, board) => {
+    if(board[one] === symbol && board[two] === symbol && board[three] === symbol){
+      declareWinner(symbol);
+    }
+  }
+  
+  const declareWinner = (winner)=>{
+    const para = document.querySelector("#turnPara");
+    para.textContent = `${winner} won!`;
+    const gameBoard = document.querySelector("#gameBoard");
+    Array.from(gameBoard.children).forEach(element => {
+      element.disabled = true;
+    });
+  }
+  
+  const checkDraw = () => {
+    const para = document.querySelector("#turnPara");
+    let filled = true;
+    const gameBoard = document.querySelector("#gameBoard");
+    Array.from(gameBoard.children).forEach(element => {
+      if(element.textContent === ""){
+        filled = false;
+      }
+    });
+    if(filled){
+      para.textContent = "It's a draw!";
+    } 
+  }
+
   for(let i=0 ; i<9;i++){
     const tile = document.createElement('button');
     tile.addEventListener('click', function(){
@@ -24,76 +97,7 @@ let Game = (function(){
     gameBoard.append(tile);
   };
 
-})();
-setId();
+  setId();
+};
 
-function toDisplay(p1, p2, piece, board){
-  pyTurn = playerTurn(p1, p2);
-  piece.textContent = pyTurn;
-  board[piece.getAttribute('id')] = pyTurn;
-  piece.disabled = true;
-  checkDraw();
-  checkWin(p1, p2, board);
-}
-
-function playerTurn(one, two){
-  return one.isTurn?changeTurn(one, two): changeTurn(two, one);
-}
-
-function changeTurn(player1, player2){
-  const turnPara = document.querySelector("#turnPara");
-  player1.isTurn = false;
-  player2.isTurn = true;
-  turnPara.textContent = player2.symbol+"'s turn!";
-  return player1.symbol;
-}
-
-function checkWin(one, two, board){
-  const win = [
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for(let i = 0; i<win.length; i++){
-    let a = win[i][0];
-    let b = win[i][1];
-    let c = win[i][2];
-    symbolCheck(a, b, c, one.symbol, board);
-    symbolCheck(a, b, c, two.symbol, board);
-  }
-}
-
-function symbolCheck(one, two , three, symbol, board){
-  if(board[one] === symbol && board[two] === symbol && board[three] === symbol){
-    declareWinner(symbol);
-  }
-}
-
-function declareWinner(winner){
-  const para = document.querySelector("#turnPara");
-  para.textContent = `${winner} won!`;
-  const gameBoard = document.querySelector("#gameBoard");
-  Array.from(gameBoard.children).forEach(element => {
-    element.disabled = true;
-  });
-}
-
-function checkDraw(){
-  const para = document.querySelector("#turnPara");
-  let filled = true;
-  const gameBoard = document.querySelector("#gameBoard");
-  Array.from(gameBoard.children).forEach(element => {
-    if(element.textContent === ""){
-      filled = false;
-    }
-  });
-  if(filled){
-    para.textContent = "It's a draw!";
-  } 
-}
+Game();
